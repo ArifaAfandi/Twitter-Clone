5000c3580fe84a217a7041bc8275aa7dac340a8e
Arifa Afandieva <63201336+ArifaAfandi@users.noreply.github.com>
2022-09-01 17:02:05 +0400

    fixed errors

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,7 +6,7 @@ const signin = ({ providers }) => {
             <img className="hidden object-cover rotate-6 md:w-44 md:h-80 md:inline-flex" src="https://cdn.cms-twdigitalassets.com/content/dam/blog-twitter/archive/twitter_alerts_newcountriesandfeatures95.thumb.1280.1280.png" alt="twitter's background" />
             <div>
                 {Object.values(providers).map((provider) => (
-                    <div className="flex flex-col items-center">
+                    <div key={provider.name} className="flex flex-col items-center">
                         <img className="w-36 object-cover" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1200px-Twitter-logo.svg.png" alt="twitter logo" />
                         <p className="text-center text-sm italic my-10">This app is created for learning purposes</p>
                         <button onClick={() => signIn(provider.id, {callbackUrl: "/"})} className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500">Sign in with {provider.name}</button>
